5b0f25cd258527e2711f781c5ddfcd74876aed14
kawabata <kawabata@idea-PC>
2015-08-09 17:13:31 +0900

    不必要な変数をGlobalから削除しました。

--- src/com/example/test/Globals.java
+++ src/com/example/test/Globals.java
@@ -5,23 +5,13 @@ import java.util.ArrayList;
 import android.app.Application;
 
 public class Globals extends Application {
-	// グローバルに使用する変数たち
-	int hoge;
-	String str;
-	boolean fuga;
-	int[] hogera = new int[10];
+	// グローバルに使用する変数
 	ArrayList<String> nameList = new ArrayList<>();
 	
 	/**
 	 * global変数を初期化します
 	 */
 	public void GlobalsAllInit() {
-		this.hoge = 0;
-		this.str = ""; //$NON-NLS-1$
-		this.fuga = true;
-		for (int i = 0; i < this.hogera.length; i++) {
-			 this.hogera[i] = 0;
-		}
 		if(this.nameList.size() != 0){
 			this.nameList.clear();			
 		}

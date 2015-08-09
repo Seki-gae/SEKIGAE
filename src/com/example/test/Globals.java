6e039ee63ac07943d904eecf949068cd0b3443ea
kawabata <kawabata@idea-PC>
2015-08-09 16:16:58 +0900

    globalにLIST変数を追加しました

--- src/com/example/test/Globals.java
+++ src/com/example/test/Globals.java
@@ -1,5 +1,7 @@
 package com.example.test;
 
+import java.util.ArrayList;
+
 import android.app.Application;
 
 public class Globals extends Application {
@@ -8,14 +10,18 @@ public class Globals extends Application {
 	String str;
 	boolean fuga;
 	int[] hogera = new int[10];
-
-	// ぜんぶ初期化するメソッド
+	ArrayList<String> nameList = new ArrayList<>();
+	
+	/**
+	 * global変数を初期化します
+	 */
 	public void GlobalsAllInit() {
 		this.hoge = 0;
-		this.str = "あいうえお";
+		this.str = ""; //$NON-NLS-1$
 		this.fuga = true;
 		for (int i = 0; i < this.hogera.length; i++) {
 			 this.hogera[i] = 0;
 		}
+		this.nameList.clear();
 	}
 }

4fda65d0c5d5a9c27abb19375500faedc08a8199
kawabata <kawabata@idea-PC>
2015-08-10 17:26:58 +0900

    色が変わるようになりました

--- src/com/example/test/Human.java
+++ src/com/example/test/Human.java
@@ -2,6 +2,8 @@ package com.example.test;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.test.IsolatedContext;
+import android.view.View;
 import android.widget.ImageView;
 
 public class Human {
@@ -11,8 +13,18 @@ public class Human {
 	public Human(Context context) {
 		// TODO Auto-generated constructor stub
 		this.imageView = new ImageView(context);
+		this.imageView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// イメージ画像がクリックされたときに実行される処理
+				if (Human.this.isMan()) {
+					Human.this.setWoman();
+				} else {
+					Human.this.setMan();
+				}
+			}
+		});
 	}
-
 	public void setMan() {
 		this.imageView.setImageResource(R.drawable.bule);
 		this.isMan = true;
@@ -22,8 +34,12 @@ public class Human {
 		this.imageView.setImageResource(R.drawable.pink);
 		this.isMan = false;
 	}
-	
-	public ImageView getImageView(){
+
+	public ImageView getImageView() {
 		return this.imageView;
+	}
+
+	public Boolean isMan() {
+		return isMan;
 	}
 }

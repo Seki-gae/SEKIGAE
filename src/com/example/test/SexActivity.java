77e4d2e1f19d85d28695a5b70c97991d6091902f
kawabata <kawabata@idea-PC>
2015-08-10 15:15:27 +0900

    新しくアクティビティを追加しました

---
+++ src/com/example/test/SexActivity.java
@@ -0,0 +1,45 @@
+package com.example.test;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * @author kawabata
+ *
+ */
+public class SexActivity extends Activity implements OnClickListener {
+	// グローバル変数
+	Globals globals;
+	// ボタン
+	Button button;
+
+	EditText editText;
+	private int num;
+
+	public static int MEMBER_NUM = 4;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		System.out.println("ACTIVITY ONCLEATE"); //$NON-NLS-1$
+		super.onCreate(savedInstanceState);
+		// レイアウト定義
+		setContentView(R.layout.activity_sex);
+		// インテントを取得
+		Intent intent = getIntent();
+		// インテントに保存されたデータを取得
+		this.num = intent.getIntExtra("NUM", -1);
+		// グローバル変数を取得
+		this.globals = (Globals) this.getApplication();
+
+	}
+
+	@Override
+	public void onClick(View v) {}
+
+}

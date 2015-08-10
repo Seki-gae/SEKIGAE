81897a13dd6f92761d193b15cf379248f9aa5735
kawabata <kawabata@idea-PC>
2015-08-10 17:31:11 +0900

    Global変数にリストを追加しました

--- src/com/example/test/Globals.java
+++ src/com/example/test/Globals.java
@@ -1,6 +1,5 @@
 package com.example.test;
 
-
 import java.util.ArrayList;
 
 import android.app.Application;
@@ -9,14 +8,15 @@ public class Globals extends Application {
 	// グローバルに使用する変数
 	ArrayList<String> nameList = new ArrayList<>();
 	ArrayList<Integer> m_member_num = new ArrayList<>();
-	ArrayList<Integer> w_member_num = new ArrayList<>();	
-	
+	ArrayList<Integer> w_member_num = new ArrayList<>();
+	ArrayList<Boolean> isManList = new ArrayList<>();
+
 	/**
 	 * global変数を初期化します
 	 */
 	public void GlobalsAllInit() {
-		if(this.nameList.size() != 0){
-			this.nameList.clear();			
+		if (this.nameList.size() != 0) {
+			this.nameList.clear();
 		}
 
 	}

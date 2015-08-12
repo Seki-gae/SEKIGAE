2d0ac8cbe7c08868ec95cb707125dda1ec289831
kawabata <kawabata@idea-PC>
2015-08-12 20:59:10 +0900

    まるの多きさを変えました

--- src/com/example/test/ResultActivity.java
+++ src/com/example/test/ResultActivity.java
@@ -18,26 +18,25 @@ import java.util.ArrayList;
 
 import com.example.mock.*;
 
-public class ResultActivity extends Activity implements OnClickListener{
+public class ResultActivity extends Activity implements OnClickListener {
 	private Globals globals;
 	private int num;
 	private LinearLayout left;
 	private LinearLayout right;
-	
+
 	private ArrayList<String> alList = new ArrayList<>();
 	ArrayList<Member> array = new ArrayList<Member>();
 	private ArrayList<ManOrWoman> leftImages = new ArrayList<>();
 	private ArrayList<ManOrWoman> rightImages = new ArrayList<>();
-	
-	
+
 	private Object imageView;
 	private Context context;
 	public static int MAN_NUM = 3;
 	public static int WOMAN_NUM = 3;
-	
+
 	public static int left_NUM = 3;
 	public static int right_NUM = 3;
-	
+
 	protected void onCreate(Bundle savedInstanceState) {
 		System.out.println("ACTIVITY ONCLEATE"); //$NON-NLS-1$
 		super.onCreate(savedInstanceState);
@@ -51,23 +50,23 @@ public class ResultActivity extends Activity implements OnClickListener{
 		this.globals = (Globals) this.getApplication();
 		// レイアウトの読み込み
 		this.left = (LinearLayout) findViewById(R.id.LinearLayout_left_result1);
-		this.right = (LinearLayout) findViewById(R.id.LinearLayout_right_result1);//親Viewを取得してremoveView実行
+		this.right = (LinearLayout) findViewById(R.id.LinearLayout_right_result1);// 親Viewを取得してremoveView実行
 		this.left.setGravity(Gravity.CENTER);
 		this.right.setGravity(Gravity.CENTER);
-	
-//		ArrayList<Member> right_array = Mock.getRightMembers();
-//		ArrayList<Member> left_array = Mock.getLeftMembers();
-		ArrayList<Member> right_array =this.globals.rightList;
+
+		// ArrayList<Member> right_array = Mock.getRightMembers();
+		// ArrayList<Member> left_array = Mock.getLeftMembers();
+		ArrayList<Member> right_array = this.globals.rightList;
 		ArrayList<Member> left_array = this.globals.leftList;
-		
+
 		WindowManager wm = getWindowManager();
 		Display disp = wm.getDefaultDisplay();
 		LinearLayout.LayoutParams param1 = new LinearLayout.LayoutParams(
-				disp.getHeight() / 9, LayoutParams.FILL_PARENT);
-		param1.weight =1.0f;
-		//this.imageView = new ImageView(context);
-		
-		int count=0;
+				disp.getHeight() / 6, disp.getHeight() / 6);
+		param1.weight = 1.0f;
+		// this.imageView = new ImageView(context);
+
+		int count = 0;
 		for (Member member : left_array) {
 			ManOrWoman manOrWoman = new ManOrWoman(this);
 			manOrWoman.setName(member);
@@ -78,13 +77,11 @@ public class ResultActivity extends Activity implements OnClickListener{
 			ManOrWoman manOrWoman = new ManOrWoman(this);
 			manOrWoman.setName(member);
 			this.rightImages.add(manOrWoman);
-			this.right.addView(manOrWoman.getImageView(), param1);			
+			this.right.addView(manOrWoman.getImageView(), param1);
 		}
 
-		
 	}
-	
-	
+
 	@Override
 	public void onClick(View v) {
 		this.alList.add("A");
@@ -92,7 +89,7 @@ public class ResultActivity extends Activity implements OnClickListener{
 		this.alList.add("C");
 		this.alList.add("D");
 		this.alList.add("E");
-		
+
 		int mCount = 0, wCount = 0;
 		for (ManOrWoman manOrWoman : leftImages) {
 			Member member = new Member();
@@ -105,8 +102,8 @@ public class ResultActivity extends Activity implements OnClickListener{
 				member.setName(this.alList.get(wCount));
 				wCount++;
 			}
-			
-			for (ManOrWoman manOrWoman2: rightImages) {
+
+			for (ManOrWoman manOrWoman2 : rightImages) {
 				Member member2 = new Member();
 				if (manOrWoman2.isMan) {
 					member2.setGender("man");
@@ -121,8 +118,7 @@ public class ResultActivity extends Activity implements OnClickListener{
 				intent = new Intent(ResultActivity.this, MainActivity.class);
 				// アクティビティの起動
 				startActivity(intent);
-			}		
+			}
 		}
 	}
 }
-	
